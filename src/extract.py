e9ea3e466cc0e5261f503bd8832de94b4f3176f6
Thiago <thiago.giachetto@gmail.com>
2020-08-09 20:42:16 -0300

    script to change data format of dataset to csv

---
+++ src/extract.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import re
+import sys
+
+def txtToCSV(filepath, outPath):
+    i=0
+    with open(filepath, "r") as file:
+        with open(outPath, "w+") as out:
+            for idx, line in enumerate(file):
+                splitedLine = re.split("[ ]+", line)[1:]
+                if idx == 0:
+                    out.write(f"class,{','.join([str(i) for i in range(len(splitedLine))])}\n")
+                out.write(",".join(splitedLine))
+                i += 1
+
+if __name__ == '__main__':
+    filepath = sys.argv[1]
+    out_filepath = sys.argv[2]
+
+    
+    txtToCSV(filepath, out_filepath)
+
